ba9880b4136265276a80948b2974b3f86864208b
Renan Souza <renan.rodrigues.souza1@gmail.com>
2023-09-11 16:23:47 -0300

    Fixes (#4)
    * import path fix
    
    * fix macro

--- bin/header.h
+++ bin/header.h
@@ -4,14 +4,14 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#include "../../static_utils/struct.h"
+#include "../static_utils/struct.h"
 
 handler_p mem_handler_alloc(size_t size, char const func[]);
 void mem_handler_free(handler_p h);
 bool mem_empty();
 void mem_report();
 
-#define malloc(SIZE) mem_handler_alloc(SIZE, __FILE__, __LINE__)
+#define malloc(SIZE) mem_handler_alloc(SIZE, __func__)
 #define free(HANDLER) mem_handler_free(HANDLER)
 
 #endif

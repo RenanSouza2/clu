f4117074cdbb26e9aebf6a5144512c3c22b6580d
Renan Souza <renan.rodrigues.souza1@gmail.com>
2023-10-09 13:52:03 -0300

    assert free (#11)

--- bin/header.h
+++ bin/header.h
@@ -3,11 +3,12 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <assert.h>
 
 typedef void * handler_p;
 
 handler_p mem_handler_alloc(size_t size, char format[], ...);
-void mem_handler_free(handler_p h, ...);
+bool mem_handler_free(handler_p h, char format[], ...);
 
 bool mem_empty();
 void mem_report(char tag[]);
@@ -15,6 +16,6 @@ void mem_report_full(char tag[]);
 handler_p mem_get_pointer(int x, int y);
 
 #define malloc(SIZE) mem_handler_alloc(SIZE, "f|%s|l|%d", __func__, __LINE__)
-#define free(HANDLER) mem_handler_free(HANDLER)
+#define free(HANDLER) assert(mem_handler_free(HANDLER, "f|%s|l|%d", __func__, __LINE__))
 
 #endif

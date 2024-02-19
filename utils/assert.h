b3ffb702fde273956d0155de459c41604157b98d
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2024-02-19 19:07:45 -0300

    fix compatibility

--- utils/assert.h
+++ utils/assert.h
@@ -5,11 +5,25 @@
 #include <assert.h>
 
 #undef  assert
+
+#ifdef __linux__
+
 #define assert(COND)        \
     if(!(COND))             \
     {                       \
         printf("\n\n");     \
         __assert_fail(#COND, __FILE__, __LINE__, __ASSERT_FUNCTION);    \
     }
+
+#elif defined __APPLE__
+
+#define assert(COND)        \
+    if(!(COND))             \
+    {                       \
+        printf("\n\n");     \
+        __assert_rtn(__func__, __ASSERT_FILE_NAME, __LINE__, #COND);    \
+    }
+
+#endif
 
 #endif

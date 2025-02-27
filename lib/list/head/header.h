df588063016c68c24fecbb9f8cde94816c98cffc
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2025-02-27 17:29:42 -0300

    --

--- lib/list/head/header.h
+++ lib/list/head/header.h
@@ -16,7 +16,6 @@ void clu_list_head_free(list_head_p *lh_root);
 bool clu_list_head_insert(list_head_p *lh_root, tag_p tag, handler_p h);
 bool clu_list_head_remove(list_head_p *lh_root, handler_p h);
 
-
 int clu_list_head_count(list_head_p lh);
 list_body_p clu_list_head_get_body(list_head_p lh, int x);
 bool clu_list_head_contains(list_head_p lh, handler_p h);

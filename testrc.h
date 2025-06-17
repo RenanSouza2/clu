088f6974aa726c20d9cb9009a8b7267d1a7e2a2c
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2025-06-17 11:02:19 -0300

    update macro

--- testrc.h
+++ testrc.h
@@ -1,5 +1,5 @@
-#include "./lib/mem/header.h"
-#include "./mods/macros/assert.h"
+#include "lib/mem/header.h"
+#include "mods/macros/assert.h"
 
 #define TEST_ASSERT_MEM_EMPTY assert(clu_mem_internal_empty());
 #define TEST_CASE_TIMEOUT_MS 500

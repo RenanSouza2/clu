fb76e353ef1727eb195d7c0df3606b3ee9b1f5c2
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2026-02-05 19:29:13 -0300

    flags

--- makefiles/lib.mk
+++ makefiles/lib.mk
@@ -1,13 +1,6 @@
-FLAGS = -std=c11 -Wall -Wextra -Werror -Wfatal-errors -Wpedantic -Wshadow -Wpointer-arith -Wcast-qual -Wstrict-prototypes -Wformat=2 -Wnull-dereference -Wconversion -Wsign-conversion -D_POSIX_C_SOURCE=200809L
-FLAGS_PRD = -O2 -march=native -fstack-protector-strong
-FLAGS_DBG = -D DEBUG -O0 -g3 -ggdb -fno-omit-frame-pointer -fsanitize=address,undefined
+include $(shell git rev-parse --show-toplevel)/makefiles/flags.mk
 
-ifeq ($(shell uname -s),Linux)
-    FLAGS_PRD += -fstack-clash-protection
-	FLAGS_DBG += -fsanitize=leak
-endif
-
-LIB_ROOT = $(shell git rev-parse --show-toplevel 2>/dev/null)/lib
+LIB_ROOT = $(shell git rev-parse --show-toplevel)/lib
 DIR = $(shell basename $(CURDIR))
 
 

efb5d8649f52540490ee7171bb9ab8be9207e16d
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2026-02-07 11:25:06 -0300

    improve vars

--- makefiles/flags.mk
+++ makefiles/flags.mk
@@ -1,5 +1,5 @@
 FLAGS = -std=c11 -Wall -Wextra -Werror -Wfatal-errors -Wpedantic -Wshadow -Wpointer-arith -Wcast-qual -Wstrict-prototypes -Wwrite-strings -Wundef -Wformat=2 -Wnull-dereference -Wconversion -Wsign-conversion -D_POSIX_C_SOURCE=200809L
-FLAGS_PRD = -O2 -march=native -fstack-protector-strong -D_FORTIFY_SOURCE=2 -flto
+FLAGS_PRD = -O2 -march=native -fstack-protector-strong -D_FORTIFY_SOURCE=2
 FLAGS_DBG = -D DEBUG -O0 -g3 -ggdb -fno-omit-frame-pointer -fsanitize=address,undefined
 
 ifeq ($(shell uname -s),Linux)

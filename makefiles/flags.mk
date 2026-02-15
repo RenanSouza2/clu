c4c63a1d5560a330152cde6e302cb3c90b653fbf
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2026-02-15 09:37:40 -0300

    more flags

--- makefiles/flags.mk
+++ makefiles/flags.mk
@@ -1,4 +1,4 @@
-FLAGS = -std=c11 -Wall -Wextra -Wpedantic -fanalyzer -Werror -Wfatal-errors -Wshadow -Wpointer-arith -Wcast-qual -Wstrict-prototypes -Wwrite-strings -Wundef -Wformat=2 -Wnull-dereference -Wconversion -Wsign-conversion -D_POSIX_C_SOURCE=200809L -Wimplicit-fallthrough -Wduplicated-cond -Wduplicated-branches -Wlogical-op -Wfloat-equal -fPIE
+FLAGS = -std=c11 -Wall -Wextra -Wpedantic -fanalyzer -Werror -Wfatal-errors -Wshadow -Wpointer-arith -Wcast-qual -Wstrict-prototypes -Wwrite-strings -Wundef -Wformat=2 -Wnull-dereference -Wconversion -Wsign-conversion -D_POSIX_C_SOURCE=200809L -Wimplicit-fallthrough -Wduplicated-cond -Wduplicated-branches -Wlogical-op -Wfloat-equal -fPIE -Wredundant-decls -Wstack-usage=1024 -Wdouble-promotion -Wmissing-include-dirs -Wswitch-enum -Wnested-externs
 FLAGS_PRD = -O2 -march=native -fstack-protector-strong -D_FORTIFY_SOURCE=2
 FLAGS_DBG = -D DEBUG -O0 -g3 -ggdb -fno-omit-frame-pointer -fsanitize=address,undefined
 FLAGS_LNK = -r -nostdlib
--- makefiles/test.mk
+++ makefiles/test.mk
@@ -1,10 +1,12 @@
+FLAGSs = $(filter-out -Wstack-usage=1024,$(FLAGS))
+
 test: runner
 	echo "running test $(DIR)"
 	./runner
 
 runner: test.c $(DBG_FILE)
 	echo "building test $(DIR)"
-	gcc -o $@ $^ $(FLAGS) $(FLAGS_DBG) $(FLAGS_EXE)
+	gcc -o $@ $^ $(FLAGSs) $(FLAGS_DBG) $(FLAGS_EXE)
 
 clean:
 	rm -rf runner
